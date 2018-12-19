830d7ecf8b3cf013dfbf85d843292166fb3f6e95
Brandon Villa <bornofos@gmail.com>
2018-12-18 22:50:17 -0600

    use prima instance to create and read all Links from database

---
+++ src/use-prisma-instance.js
@@ -0,0 +1,16 @@
+const { prisma } = require('./generated/prisma-client')
+
+async function main() {
+  // Create a new Link
+  const newLink = await prisma.createLink({
+    url: 'www.graphql.com',
+    description: 'Best GraphQL tutorial'
+  })
+
+  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
+  // Read all links from the database and print them to console
+  const allLinks = await prisma.links()
+  console.log(allLinks)
+}
+
+main().catch(err => console.error(err))
